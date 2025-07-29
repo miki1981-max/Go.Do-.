0446dbaf70a55df1c8661801e781b8b364b0a78f
miki1981-max <miki43154@gmail.com>
2025-07-29 13:33:14 +0200

    v1.00.005 (update animated splash Screen)

--- components/SplashScreen/Splash.Screen.tsx
+++ components/SplashScreen/Splash.Screen.tsx
@@ -1,60 +1,119 @@
 import { useEffect, useRef, useState } from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Animated, Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const size = Dimensions.get('window');
 const sizeWidth = size.width;
+const initialTop = size.height * 0.15;
+const goWidth = 220;
+const initialLeft = (sizeWidth - goWidth) / 2;
 
 export default function SplashScreen() {
     const scrollRef = useRef<any>(null);
     const [isOpen, setIsOpen] = useState(false);
+
+    // animation Do.
+    const opacity = useRef(new Animated.Value(1)).current;
+    // animation Bg
+    const background = useRef(new Animated.Value(0)).current;
+
+    const interpolatedBackroud = background.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['rgba(255,255,0,1)', 'rgba(255,255,0,0)']
+    })
+    // animation Go
+    const top = useRef(new Animated.Value(initialTop)).current;
+    const left = useRef(new Animated.Value(initialLeft)).current;
+    const fontSizeGo = useRef(new Animated.Value(150)).current;
+
+
+
+
+
+
+
     useEffect(() => {
         setTimeout(() => {
-            scrollRef.current.scrollTo({
-                x : sizeWidth,
-                animated : true
+            scrollRef.current?.scrollTo({
+                x: sizeWidth,
+                animated: true
             })
         }, 1000)
         setTimeout(() => {
-            scrollRef.current.scrollTo({
-                x : sizeWidth * 2,
-                animated : true
+            scrollRef.current?.scrollTo({
+                x: sizeWidth * 2,
+                animated: true
             })
         }, 2000)
         setTimeout(() => {
-            scrollRef.current.scrollTo({
-                x : sizeWidth * 3,
-                animated : true
+            scrollRef.current?.scrollTo({
+                x: sizeWidth * 3,
+                animated: true
             })
         }, 3000)
         setTimeout(() => {
-            scrollRef.current.scrollTo({
-                x : sizeWidth * 4,
-                animated : true
+            scrollRef.current?.scrollTo({
+                x: sizeWidth * 4,
+                animated: true
             })
         }, 4000)
         setTimeout(() => {
-            scrollRef.current.scrollTo({
-                x : sizeWidth * 5,
-                animated : true
+            scrollRef.current?.scrollTo({
+                x: sizeWidth * 5,
+                animated: true
             })
         }, 5000)
         setTimeout(() => {
-            setIsOpen(true)
+            // setIsOpen(true)
+            // Animated.sequence([]).start()
+            // Animated.sequence([]).start()
+            //  Animated.sequence([]).start()
+            Animated.parallel([
+                Animated.timing(opacity, {
+                    toValue: 0,
+                    duration: 300,
+                    useNativeDriver: true
+                }),
+                Animated.timing(background, {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: false
+                }),
+                Animated.timing(top, {
+                    toValue: 18,
+                    duration: 300,
+                    useNativeDriver: false
+                }),
+                Animated.timing(fontSizeGo, {
+                    toValue: 36,
+                    duration: 300,
+                    useNativeDriver: false
+                }),
+                Animated.timing(left, {
+                    toValue: 16,
+                    duration: 300,
+                    useNativeDriver: false
+                }),
+            ]).start()
         }, 5200)
-    },[])
+        setTimeout(() => {
+            setIsOpen(true)
+        },5500)
+    }, [])
 
-    if(isOpen) return null;
-    return (
-        <View style={styles.global}>
-            <Text style={styles.textGo}>Go.</Text>
+    // if (isOpen) return null;
+    return (<>
+        <Animated.Text style={[styles.textGo, { top, fontSize: fontSizeGo, left }]}>Go.</Animated.Text>
+        <Animated.View style={[styles.global, { backgroundColor: interpolatedBackroud, display : isOpen ? 'none' : 'flex' }]}>
+            <Text style={styles.textGo2}>Go.</Text>
             <ScrollView ref={scrollRef} style={styles.scrollC} scrollEnabled={false} horizontal={true} contentContainerStyle={styles.scrollCont}>
-            <Text style={styles.textPlay}>Play</Text>
-            <Text style={styles.textSee}>See</Text>
-            <Text style={styles.textHear}>Hear</Text>
-            <Text style={styles.textLive}>Live</Text>
-            <Text style={styles.textDo}>Do.</Text>
+                <Text style={styles.textPlay}>Play</Text>
+                <Text style={styles.textSee}>See</Text>
+                <Text style={styles.textHear}>Hear</Text>
+                <Text style={styles.textLive}>Live</Text>
+                <Animated.Text style={[styles.textDo, { opacity }]}>Do.</Animated.Text>
             </ScrollView>
-        </View>
+        </Animated.View>
+    </>
     )
 }
 const styles = StyleSheet.create({
@@ -67,63 +126,72 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: "flex-start",
         alignItems: "center",
-
         zIndex: 99
     },
     textGo: {
+        // fontSize: 150,
+        fontWeight: 700,
+        position: 'absolute',
+        zIndex: 999
+
+        // top : '15%',
+    },
+    textGo2: {
         fontSize: 150,
-        fontWeight:700,
-        marginTop : '30%',
+        fontWeight: 700,
+        marginTop: '30%',
+        color: 'rgba(0,0,0,0)'
     },
-    scrollC :{
-        marginTop : '25%',
-       display : 'flex',
-       
-       width : '100%',
-       height : 180,
-       maxHeight : 180
-    },
-    scrollCont : {
+    scrollC: {
+        marginTop: '25%',
         display: 'flex',
-        flexDirection : 'row',
-        alignItems : 'flex-start'
+
+        width: '100%',
+        height: 180,
+        maxHeight: 180
     },
-    textPlay:{
-        fontSize: 150,
-        fontWeight:700,
-        color:"green",
-        width :sizeWidth,
-        textAlign  :'center',
+    scrollCont: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start'
     },
-    textSee:{
+    textPlay: {
         fontSize: 150,
-        fontWeight:700,
-        color:"red",
-        width :sizeWidth,
-        textAlign  :'center',
+        fontWeight: 700,
+        color: "green",
+        width: sizeWidth,
+        textAlign: 'center',
     },
-    textHear:{
+    textSee: {
         fontSize: 150,
-        fontWeight:700,
-        color:"purple",
-        width :sizeWidth,
-        textAlign  :'center',
+        fontWeight: 700,
+        color: "red",
+        width: sizeWidth,
+        textAlign: 'center',
+    },
+    textHear: {
+        fontSize: 150,
+        fontWeight: 700,
+        color: "purple",
+        width: sizeWidth,
+        textAlign: 'center',
     },
 
-    textLive:{
+    textLive: {
         fontSize: 150,
-        fontWeight:700,
-        color:"blue",
-        width :sizeWidth,
-        textAlign  :'center',
+        fontWeight: 700,
+        color: "blue",
+        width: sizeWidth,
+        textAlign: 'center',
     },
-    textDo:{
+    textDo: {
         fontSize: 150,
-        fontWeight:700,
-        color:"black",
-        width :sizeWidth,
-        textAlign  :'center',
-        
+        fontWeight: 700,
+        color: "black",
+        width: sizeWidth,
+        textAlign: 'center',
+        // position: 'absolute'
+
     },
 
 });
